7d18f503a826834c3254a33cf3709f0034c3a452
dhanurefian-hue <dhanurefian@gmail.com>
2026-06-05 16:03:32 +0700

    belajar day 2 p1

--- day1/04/06/2006.py
+++ day1/04/06/2006.py
@@ -7,3 +7,19 @@ print(b)
 tahun = 2025 
 tahun_sekarang = (tahun + 1)
 print(tahun_sekarang)
+#soal 3
+nama = "dhanu"
+umur = 17
+ipk = 3.75
+aktif = True
+print(str(nama))
+print(int(umur))
+print(float(ipk))
+print(bool(aktif))
+#soal 4
+harga = 15000
+jumalah = 3
+total_harga = harga * jumalah
+print(total_harga)
+#soal 5
+print(f"hello {nama}, umur kamu {umur} tahun ")
