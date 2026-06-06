a299802d5f5829f9a31dc81a864eb656e861396f
dhanurefian-hue <dhanurefian@gmail.com>
2026-06-06 23:07:57 +0700

    soalpart2

--- day2.py
+++ day2.py
@@ -2,10 +2,20 @@
 for i in range(1, 6):
     print(i)
 #soal2
-for i in range(1, 11, 1):
+for i in range(2, 11, 2):
     print(i)
 #soal3
 hitung = 5
 while hitung >=1:
     print(hitung)
     hitung -= 1
+    #soal 1
+    nilai = [80, 75, 90, 85, 60]
+for x in nilai:
+    print(x)
+#soal 2
+total = 0
+for x in nilai:
+    total += x
+print(total)
+print(total/len(nilai))
