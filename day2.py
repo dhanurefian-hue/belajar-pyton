5756f38ec04098d5373c69ff651e11fffb3bd520
dhanurefian-hue <dhanurefian@gmail.com>
2026-06-06 22:52:07 +0700

    tugas malam 06062026

--- day2.py
+++ day2.py
@@ -1,3 +1,11 @@
-
-nama = input("Siapa nama kamu? ")
-print(nama)
+#soal1
+for i in range(1, 6):
+    print(i)
+#soal2
+for i in range(1, 11, 1):
+    print(i)
+#soal3
+hitung = 5
+while hitung >=1:
+    print(hitung)
+    hitung -= 1
